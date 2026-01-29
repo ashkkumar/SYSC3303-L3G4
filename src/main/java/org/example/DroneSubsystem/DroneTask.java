cc32b0c3bc4f7877aa54cedca96dfebc0f37dcc4
ashkkumar <ashkuma5@gmail.com>
2026-01-29 14:28:47 -0500

    Okayyy properly got this in now, seperated classes. Added a DroneTask.java file which is a class that defines a droneTask object (more convinient for later instead of passing it strings or smt). DroneStatus.java -> description of what the drone is currently doing (idle, traveling, etc). Also included a taskType enum class (only one task in it so far can add more later). And spruced up the Drone class to fit with those changes.

---
+++ src/main/java/org/example/DroneSubsystem/DroneTask.java
@@ -0,0 +1,35 @@
+package org.example.DroneSubsystem;
+
+public class DroneTask {
+
+        private final FireEvent fireEvent;
+        private final TaskType taskType;
+
+    /**
+     * Creates a Drone Task
+     * @param fireEvent
+     * @param taskType
+     */
+    public DroneTask(FireEvent fireEvent, TaskType taskType) {
+            this.fireEvent = fireEvent;
+            this.taskType = taskType;
+        }
+
+    /**
+     * Returns the fire event
+     * @return the fireEvent
+     */
+    public FireEvent getFireEvent() {
+            return fireEvent;
+        }
+
+    /**
+     * Returns the task type
+      * @return the taskType
+     */
+    public TaskType getTaskType() {
+        return taskType;
+    }
+
+
+}
---
+++ src/main/java/org/example/DroneSubsystem/TaskType.java
@@ -0,0 +1,5 @@
+package org.example.DroneSubsystem;
+
+public enum TaskType {
+    FIRE_RESPONSE
+}
